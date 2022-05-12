fe2b9a4afbe4370bd2db3793496599525ccffdce
Hassan <hassansmallah@gmail.com>
2022-05-12 14:24:22 +0300

    key

--- drf/drf/settings.py
+++ drf/drf/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1ejq4km1vmx0m3u8%5qe=b0ciz!n%sn-y(r7utv(l9%lt%ual!'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
