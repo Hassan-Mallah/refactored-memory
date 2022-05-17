7d82b33a120671f1f6fe9e8d135ffff7409489af
Hassan <hassansmallah@gmail.com>
2022-05-17 09:12:14 +0300

    broken :(

--- drf/drf/asgi.py
+++ drf/drf/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for drf project.
+ASGI config for drf drf.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
--- drf/drf/wsgi.py
+++ drf/drf/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for drf project.
+WSGI config for drf drf.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
